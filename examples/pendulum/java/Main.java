d657a2141131910d388059c91edf846e0e55a649
EdwardKN <edward.klinghammer@gmail.com>
2024-11-03 13:29:20 +0100

    jävla skit

--- examples/pendulum/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        TrainingWindow trainingWindow = new TrainingWindow();
-
-        //new NetworkDraw(Population.importFromJson("test.json").getBestSamplesSorted(1)[0].getNetwork());
-    }
-}
